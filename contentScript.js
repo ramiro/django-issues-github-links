eeef100a1fae83bfe33fc6d3ef80810820cf795a
Ramiro Morales <cramm0@gmail.com>
2019-09-09 09:04:43 -0300

    Start debugging & fixing multiple tokens.
    It must detect and linkify paraphaphs like
    ```
    Fixed #aaaa, #bbbb. More text here...
    ```
    but not mre than that. e.g.
    ```
    Fixed #aaa, #bbb. More text here potentially including a GitHub PR #ccc
    ```
    the token #ccc above must not be linkified.
    
    Also, ee must make sure we keep procesing correctly things like:
    ```
    Fixed #aaa, Refs #bbb. More text here...
    ```

--- contentScript.js
+++ contentScript.js
@@ -12,7 +12,7 @@
 // https://github.com/django/django/pull/11735
 
 var sentinel_re = /((refs\.?|fixe[ds]))(?=\s+#\d+)/gi;
-var ticket_re = /(,?\s+)#(\d+)/g;
+var ticket_re = /^(,?\s+)#(\d+)/g;
 // Example of a link we need to replace:
 // <a class="issue-link js-issue-link tooltipped tooltipped-ne" data-error-text="Failed to load issue title" data-id="53897017" data-permission-text="Issue title is private" data-hovercard-type="pull_request" data-hovercard-url="/django/django/pull/3871/hovercard" href="https://github.com/django/django/pull/3871" aria-label="#3871, Non digits custom user model primary key password lookup.">#3871</a>
 var github_pr_link_re = /((?:refs\.?|fixe[ds])\s+)(?:<a class="issue-link js-issue-link[^>]+?>)#(\d+)(?:<\/a>)/gi;
@@ -38,14 +38,58 @@ for (var sel of selectors) {
   var elements = document.querySelectorAll(sel);
   elements.forEach(function(e) {
     var innerHtml = e.innerHTML;
+    var work2 = innerHtml.replace(github_pr_link_re, pr_remover);
     innerHtml = innerHtml.replace(github_pr_link_re, pr_remover);
-    if (sentinel_re.exec(innerHtml)) {
-      var preamble = innerHtml.slice(0, sentinel_re.lastIndex);
-      var body = innerHtml.slice(sentinel_re.lastIndex);
-      while (ticket_re.test(body)) {
-        body = body.replace(ticket_re, replacer);
+    console.log('level 1: element HTML: "' + innerHtml + '"');
+    //var result = innerHtml;
+    //while (sentinel_re.exec(innerHtml)) {
+    while (sentinel_re.exec(work2)) {
+      var sentinel_lastidx = sentinel_re.lastIndex;
+      console.log('\t\tlevel 2: lastIndex: ' + sentinel_lastidx);
+      //var preamble = innerHtml.slice(0, sentinel_lastidx);
+      var preamble = work2.slice(0, sentinel_lastidx);
+      console.log('\tlevel 2: preamble: "' + preamble + '"');
+      //var body = innerHtml.slice(sentinel_lastidx);
+      var body = work2.slice(sentinel_lastidx);
+      console.log('\tlevel 2: body: "' + body + '"');
+      //var work = innerHtml.slice(sentinel_lastidx);
+      var work = work2.slice(sentinel_lastidx);
+      var result = preamble;
+      //result = preamble;
+      var end_marker = 0;
+      while (ticket_re.test(work)) {
+        var lastIndex = ticket_re.lastIndex;
+        console.log('\t\tlevel 3: lastIndex: ' + lastIndex);
+        //var fragment = work.slice(0, lastIndex);
+        //fragment = fragment.replace(ticket_re, replacer);
+        var fragment = work.slice(0, lastIndex).replace(ticket_re, replacer);
+        console.log('\t\tlevel 3: fragment: "' + fragment + '"');
+        result += fragment;
+        end_marker += lastIndex;
+        work = work.slice(lastIndex);
+        console.log('\t\tlevel 3: work: "' + work + '"');
       }
-      e.innerHTML = preamble + body;
+      //result += body.slice(end_marker);
+      result += work2.slice(end_marker);
+      console.log('\tlevel 2 (post): result: "' + result + '"');
+      //console.log('\tlevel 2 (post): remaining: "' + innerHtml.slice(sentinel_re.lastIndex) + '"');
+      work2 = work2.slice(lastIndex);
+      sentinel_re.lastIndex = 0;
     }
+    //e.innerHTML = innerHtml;
+    e.innerHTML = result;
   });
+}
+
+function processTicketToken(ticketRegexp, work, result) {
+  var lastIndex = ticketRegexp.lastIndex;
+  console.log('\t\tlevel 3: lastIndex: ' + lastIndex);
+  //var fragment = work.slice(0, lastIndex);
+  //fragment = fragment.replace(ticketRegexp, replacer);
+  var fragment = work.slice(0, lastIndex).replace(ticketRegexp, replacer);
+  console.log('\t\tlevel 3: fragment: "' + fragment + '"');
+  result += fragment;
+  end_marker += lastIndex;
+  work = work.slice(lastIndex);
+  console.log('\t\tlevel 3: work: "' + work + '"');
 }
